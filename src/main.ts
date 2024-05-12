e049594b9d7354ceaab6d8f39b9a488096ba0b69
khanh nguyen <khanhjoi1@gmail.com>
2024-05-12 12:54:04 +0700

    try to enable cors client and admin

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,18 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // this set up for nest know to use class ValidationPipe
+  app.enableCors({
+    origin: [
+      'https://frontedn-shop-clothes-1wwb0z8k8-khanh-nguyens-projects-8c285c85.vercel.app',
+      'https://frontend-ipz9t38y2-khanh-nguyens-projects-8c285c85.vercel.app',
+    ],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       // this will cut the field not extend in DTO define
       // whitelist: true
     }),
   );
-
-  // CORS is disabled
-  // app.enableCors({
-  //   origin: [
-  //     'https://frontedn-shop-clothes-1wwb0z8k8-khanh-nguyens-projects-8c285c85.vercel.app/',
-  //     'https://frontend-ipz9t38y2-khanh-nguyens-projects-8c285c85.vercel.app',
-  //   ],
-  // });
 
   await app.listen(3333);
 }
